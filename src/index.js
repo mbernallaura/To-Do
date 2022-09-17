621639122d030503090f11d952faa5e6c3480fba
mbernallaura <mbernallaura@gmail.com>
2022-09-16 20:55:55 -0500

    Realizar el esquema de lo que se desarrollara en el todo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { saludar } from './js/componentes';
+// import { TodoList } from './classes/todo-list.class';
+// import { Todo } from './classes/todo.class';
+import {Todo, TodoList} from './classes' //Busca el index.js, por defecto
 import './styles.css';
 
-const nombre = 'Fernando';
+const todoList = new TodoList();
+const tarea = new Todo('Aprender JS');
 
-saludar( nombre );
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
