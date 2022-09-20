3dd0374062a60855b442bba409ebaf569d56a501
mbernallaura <mbernallaura@gmail.com>
2022-09-19 20:20:44 -0500

    Crear y visualizar las tareas que escribe el usuario

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // import { TodoList } from './classes/todo-list.class';
 // import { Todo } from './classes/todo.class';
 import {Todo, TodoList} from './classes' //Busca el index.js, por defecto
+import { crearTodoHtml } from './js/componentes';
 import './styles.css';
 
-const todoList = new TodoList();
-const tarea = new Todo('Aprender JS');
+export const todoList = new TodoList();
+const tarea = new Todo('Aprender JS!!');
 
 todoList.nuevoTodo(tarea);
+// tarea.completado = true;
 
 console.log(todoList);
+
+crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,35 @@
-import '../css/componentes.css';
-// import webpacklogo from '../assets/img/webpack-logo.png';
+import { Todo } from "../classes";
+import { todoList } from "../index";
 
+const divTodoList = document.querySelector('.todo-list');
+const txtImput = document.querySelector('.new-todo');
 
-export const saludar = ( nombre = 'sin nombre' ) => {
-    console.log('Creando etiqueta h1');
+export const crearTodoHtml = (todo) =>{
+    const htmlTodo = `
+    <li class="${(todo.completado) ? 'completed': ''}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked': ''} >
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+    <input class="edit" value="Create a TodoMVC template">
+    </li> `;
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${ nombre }`;
-
-    document.body.append( h1 );
-
-    
-    // Img
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append( img );
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+    //div.firstElementChild = nos devuelve el primer hijo del elemento que estamos seleccionando en este caso el div y devolvera li
+    divTodoList.append(div.firstElementChild);
+    return div.firstElementChild;
 }
+
+//Eventos:
+//keyup = Soltar tecla
+//evento = cual tecla fue la que presiono el usuario
+txtImput.addEventListener('keyup', (evento) =>{
+    if(evento.keyCode === 13 && txtImput.value.length > 0){
+        const nuevoTodo = new Todo(txtImput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml(nuevoTodo);
+        txtImput.value = '';
+    }
+})
